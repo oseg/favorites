59e8ade07a26de541cc94f052097ee878bd2fe8b
oseg1 <oseg1@online.fr>
2020-08-26 18:15:07 +0200

    add fr translations

---
+++ languages/fr.php
@@ -0,0 +1,15 @@
+<?php
+$favorites = array(
+	// favorites widget
+	"widgets:favorites:title" => "Favoris",
+	"widgets:favorites:description" => "Montrer vos pages favorites",
+	"widgets:favorites:delete:success" => "Favori supprimé",
+	"widgets:favorites:delete:failed" => "Echec de suppression du favori",
+	"widgets:favorites:save:success" => "Favori créé",
+	"widgets:favorites:save:failed" => "Echec de création du favori",
+	"widgets:favorites:toggle:missing_input" => "Saisie manquante pour cette action",
+	"widgets:favorites:content:more_info" => "Ajouter vos pages favorites à ce widget en cliquant sur l'icone étoile dans le menu de la barre latérale.",
+	"widgets:favorites:menu:add" => "Ajouter cette page à votre widget Favoris",
+	"widgets:favorites:menu:remove" => "Enlever cette page de votre widget Favoris",
+);
+add_translation("fr", $favorites);
